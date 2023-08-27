10a59bba35e7337a441c1e1bd0ea4b40d7d44af3
Rahul Rudra <rahulrudra665@gmail.com>
2023-08-27 08:03:28 +0600

    setup commit

--- secondpart/urls.py
+++ secondpart/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import include
+from . import  views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",views.home),
+    # path("firstapp/",include('firstapp.urls')),
+    #   path("secondapp/",include('secondapp.urls')),
 ]
